275ff83fbb4363dbddf103042c4097d67f4db4f3
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2023-01-05 14:09:20 -0500

    add base class to base.py

--- tests/base.py
+++ tests/base.py
@@ -2,4 +2,7 @@ from django.test import TestCase
 from tests import helper
 
 class BaseBookTest(TestCase):
-  def setUp
+  def setUp(self) -> None:
+    self.client = self.client_class()
+    self.book_finished_list = helper.generate_random_books(10, True)
+    self.book_unfinished_list = helper.generate_random_books(10, False)
